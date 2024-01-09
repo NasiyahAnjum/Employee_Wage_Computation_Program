9232b342d5956906c0f4c72b763061dbb78022cd
Nasiyah Anjum <nasiyahanjum@gmail.com>
2024-01-09 11:02:05 +0530

    [Naisyah]: Add: Employee is Present or Absent.

--- src/com/java/employeewagecomputationprogram/Employee_Wage_Computation_Program.java
+++ src/com/java/employeewagecomputationprogram/Employee_Wage_Computation_Program.java
@@ -5,6 +5,17 @@ public class Employee_Wage_Computation_Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To Employee Wage Computation Program");
+		final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
 	}
 
 }
