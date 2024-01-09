5dbf21f5daa4d19f7b1fc41820471f893ca3b553
Nasiyah Anjum <nasiyahanjum@gmail.com>
2024-01-09 11:08:31 +0530

    [Naisyah]: Add: Calculate Daily Employee Wage.

--- src/com/java/employeewagecomputationprogram/Employee_Wage_Computation_Program.java
+++ src/com/java/employeewagecomputationprogram/Employee_Wage_Computation_Program.java
@@ -5,17 +5,20 @@ public class Employee_Wage_Computation_Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To Employee Wage Computation Program");
-		final int FULL_TIME = 1;
-        int empType = (int) (Math.random() * 100) % 2;
-        if (empType == FULL_TIME)
-        {
-            System.out.println("Employee is Present");
-
-        } else
-        {
-            System.out.println("Employee is Absent");
-
-        }
+		 final int FULL_TIME = 1;
+	        final int WAGE_PER_HR = 20;
+	        int empType = (int) (Math.random() * 100) % 2;
+	        int workingHours = 0;
+	        if (empType == FULL_TIME)
+	        {
+	            System.out.println("Employee is Present");
+	            workingHours = 8;
+	        } else
+	        {
+	            System.out.println("Employee is Absent");
+	        }
+	        int wage = workingHours * WAGE_PER_HR;
+	        System.out.println("Employee Daily Wage is " + wage);
 	}
 
 }
